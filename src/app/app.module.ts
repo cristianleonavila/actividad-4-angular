c4d60f692b9e8d06d49bdd91334f34829a5730a6
Cristian Leon <cristian.leon.avila@gmail.com>
2024-05-29 12:14:13 -0500

    Se implementa toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/shared/nav-bar/nav-bar.component';
 import { HomeComponent } from './components/home/home.component';
@@ -20,7 +22,9 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/contacto/contacto.service.ts
+++ src/app/components/contacto/contacto.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ export class ContactoService {
 
   private endpoint:string = 'https://pruebas.cubic100.com.co/rest-v2/';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private toastr: ToastrService) { }
 
   sendComment(email:string, comment: string) {
     if ( !email.length || !comment.length) return;
@@ -21,6 +22,10 @@ export class ContactoService {
       headers: {
         user: 'ws_test',
         password: 'cTEtUzF3Tzh6KU5f'}
-    }).subscribe( res => console.log(res));
+    }).subscribe( (res:any) => {
+      if ( res.success === 1 ) {
+        this.toastr.success( res.msg, '!Hecho¡');
+      }
+    });
   }
 }
